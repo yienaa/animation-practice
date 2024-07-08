2c2ee5c33dcea5b1646e3a32d1e092e26f23b429
Lena Lee <lny3251@gmail.com>
2024-07-08 18:46:30 +0900

    Remove console

--- src/gradient-border/gradient-border.js
+++ src/gradient-border/gradient-border.js
@@ -3,7 +3,6 @@ import { fetchHtml } from '../utils/fetch.js';
 export async function createGradientBorderElement() {
   try {
     const html = await fetchHtml('./src/gradient-border/gradient-border.html');
-    console.error(html)
     return html;
   } catch {
     return 'Failed to load gradient border';
